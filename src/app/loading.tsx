2e5e1a188ca731672b6c9ff6b61aed77fd3b40d7
Kopa Ralte <kopa.ralte@gmail.com>
2025-09-23 11:28:06 +0000

    change with the photo in the public folder

--- src/app/loading.tsx
+++ src/app/loading.tsx
@@ -1,11 +1,11 @@
 import { Skeleton } from "@/components/ui/skeleton";
-import Image from "next/image";
 
 export default function Loading() {
   return (
     <div className="container mx-auto px-4 py-8 md:py-12">
       <header className="flex flex-col items-center text-center mb-8 md:mb-12">
-        <Image src="/logo.png" alt="BCM Choir Programme Logo" width="64" height="64" className="mb-4" />
+        {/* eslint-disable-next-line @next/next/no-img-element */}
+        <img src="/logo.png" alt="BCM Choir Programme Logo" width="64" height="64" className="mb-4" />
         <Skeleton className="h-12 w-1/2" />
         <Skeleton className="h-6 w-3/4 mt-4" />
       </header>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,11 @@
 import { EventSchedule } from "@/components/event-schedule";
-import Image from "next/image";
 
 export default function Home() {
   return (
     <main className="min-h-screen container mx-auto px-4 py-8 md:py-12">
       <header className="flex flex-col items-center text-center mb-8 md:mb-12">
-        <Image src="/logo.png" alt="BCM Choir Programme Logo" width="64" height="64" className="mb-4" />
+        {/* eslint-disable-next-line @next/next/no-img-element */}
+        <img src="/logo.png" alt="BCM Choir Programme Logo" width="64" height="64" className="mb-4" />
         <h1 className="font-headline text-4xl md:text-5xl font-bold tracking-tight text-primary uppercase">
           BCM CHOIR PROGRAMME
         </h1>
