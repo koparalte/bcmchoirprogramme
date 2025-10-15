9846cb876428f5a263be630a8bfd9d8c0ce1ee18
Kopa Ralte <kopa.ralte@gmail.com>
2025-10-15 10:41:33 +0000

    in the first tab fetch the data from this link "https://docs.google.com/

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@ import { EventSchedule } from "@/components/event-schedule";
 import { PageHeader } from "@/components/page-header";
 import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
 
-const BCM_SHEET_URL = "https://docs.google.com/spreadsheets/d/1-U2j_G_y0k0G-n3H4t2xY4x0mJ8L3n9w5kFvX_zO_c/edit?gid=0#gid=0";
+const BCM_SHEET_URL = "https://docs.google.com/spreadsheets/d/1xeyiLqMDULNfycqE2zStdsABz_I1eXqXBvqnOqEhs3U/edit?gid=0#gid=0";
 const BCYA_SHEET_URL = "https://docs.google.com/spreadsheets/d/1NZtNfQ9-P9KCVUUj9BYbf7mIdD2t_yO5wT5j8URquKE/edit?gid=0#gid=0";
 
 export default function Home() {
