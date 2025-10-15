287e74fa8ef5827457430919c4e6e9f527393e7c
Kopa Ralte <kopa.ralte@gmail.com>
2025-10-15 10:34:14 +0000

    "https://docs.google.com/spreadsheets/d/1NZtNfQ9-P9KCVUUj9BYbf7mIdD2t_yO

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,7 @@
 import { EventSchedule } from "@/components/event-schedule";
 import { PageHeader } from "@/components/page-header";
 
-const SHEET_URL = "https://docs.google.com/spreadsheets/d/1xeyiLqMDULNfycqE2zStdsABz_I1eXqXBvqnOqEhs3U/edit?gid=0#gid=0";
+const SHEET_URL = "https://docs.google.com/spreadsheets/d/1NZtNfQ9-P9KCVUUj9BYbf7mIdD2t_yO5wT5j8URquKE/edit?gid=0#gid=0";
 
 export default function Home() {
   return (
