89fea3cadcf7920e55325150d3e10c49d2ab9f99
Kopa Ralte <kopa.ralte@gmail.com>
2025-10-29 13:13:57 +0000

    name should be the header why are you displaying it

--- src/lib/actions.ts
+++ src/lib/actions.ts
@@ -185,7 +185,7 @@ export async function getMembers(
           name: name || '',
         };
       })
-      .filter(member => member.name); // Filter out members with no name
+      .filter(member => member.name && member.name.toLowerCase() !== 'name'); // Filter out members with no name and the header
 
     return {data: members};
   } catch (err) {
