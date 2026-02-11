1a713cbce30d74ab10804101f23ff47eeacc4b5f
Kopa Ralte <kopa.ralte@gmail.com>
2026-02-11 10:18:23 +0000

    there are still yellow squares on the background of the blue and red cir

--- src/components/event-client-schedule.tsx
+++ src/components/event-client-schedule.tsx
@@ -171,6 +171,7 @@ export function EventClientSchedule({ events, allEventsForCalendar, showAllEvent
                 modifiers={modifiers}
                 modifiersStyles={modifiersStyles}
                 classNames={{
+                  cell: "h-9 w-9 text-center text-sm p-0 relative [&:has([aria-selected])]:bg-transparent focus-within:relative focus-within:z-20",
                   day_today: "bg-transparent text-foreground",
                 }}
               />
